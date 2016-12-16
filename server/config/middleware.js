c56b2ce947bd0c8d764cca0d5a360f4620f92fd7
lthai423 <lthai423@gmail.com>
2016-12-16 09:14:45 -0800

    Added body parsing for url-encoded and raw

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,11 +1,11 @@
 const morgan = require('morgan');
-const bodyParser = require('body-parser')
-  
+const bodyParser = require('body-parser');
 
 module.exports = function(app, express) {
   // logger
-  app.use(morgan('dev'));
+  // app.use(morgan('dev'));
   
   // parse application/x-www-form-urlencoded 
-  app.use(bodyParser.urlencoded({ extended: false }))
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended: true}))
 };
